fefa7149ff7a07f017ec03fa199f5ea755f64a4d
shermanturner-robinson <Shermanrob57@gmail.com>
2025-10-02 10:51:01 -0700

    update

--- src/CellPhone.java
+++ src/CellPhone.java
@@ -16,5 +16,49 @@ public class CellPhone {
     public CellPhone(Long serialNumber, String model, String carrier, String phoneNumber,
                      String owner) {
         this.serialNumber = serialNumber;
+        this.model = model;
+        this.carrier = carrier;
+        this.phoneNumber = phoneNumber;
+        this.owner = owner;
+    }
+
+    public Long getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(Long serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
     }
 }
--- src/CellPhoneApplication.java
+++ src/CellPhoneApplication.java
@@ -4,6 +4,26 @@ public class CellPhoneApplication {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         CellPhone cp = new CellPhone();
+        System.out.println("What is the serial number?");
+        cp.setSerialNumber(scanner.nextLong());
+        scanner.nextLine();
+        System.out.println("What model is the phone?");
+        cp.setModel(scanner.nextLine());
+        scanner.nextLine();
+        System.out.println("Who is the carrier?");
+        cp.setOwner(scanner.nextLine());
+        scanner.nextLine();
+        System.out.println("What is the phone number?");
+        cp.setPhoneNumber(scanner.nextLine());
+        scanner.nextLine();
+        System.out.println("Who is the owner of the phone?");
+        cp.setOwner(scanner.nextLine());
+        scanner.nextLine();
+
+        System.out.printf("The %s with serial number %d by carrier %s and phone nr %s is owned by %s.", cp.getModel(), cp.getSerialNumber(), cp.getCarrier(), cp.getPhoneNumber(), cp.getOwner());
+
+
+
 
     }
 }
