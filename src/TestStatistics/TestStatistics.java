12dbc1f1cb403cada49e6d8ee63bf7fbf8897a16
shermanturner-robinson <Shermanrob57@gmail.com>
2025-10-03 16:02:26 -0700

    commit

---
+++ src/TestStatistics/TestStatistics.java
@@ -0,0 +1,18 @@
+package TestStatistics;
+
+import java.util.Arrays;
+
+public class TestStatistics {
+    public static void main(String[] args) {
+        // Create an array of 10 test scores
+        int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        // Average
+        double sum = 0;
+        for (int i = 0; i < testScores.length; i++) {
+            sum += testScores[1];
+        }
+
+        }
+
+    }
+
